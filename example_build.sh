f9fb4e91946233aa017dcc93fe8a62d9cdfa92fb
Christopher Gorman <christophergorman@lbl.gov>
2017-07-18 12:15:13 -0700

    Made some small changes, allowing for debugging.

--- example_build.sh
+++ example_build.sh
@@ -13,15 +13,29 @@ found_host=false
 if [[ $NERSC_HOST = "edison" ]]; then
     found_host=true
     #	  -DCMAKE_CXX_FLAGS="-DUSE_TASK_TIMER -DCOUNT_FLOPS" \
-    cmake ../STRUMPACK -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../STRUMPACK-install \
+    cmake ../STRUMPACK -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=../STRUMPACK-install \
 	  -DCMAKE_CXX_COMPILER=CC -DCMAKE_C_COMPILER=cc -DCMAKE_Fortran_COMPILER=ftn \
 	  -DCMAKE_EXE_LINKER_FLAGS="-dynamic" \
-	  -DMETIS_INCLUDES=$HOME/local/edison/intel17/parmetis-4.0.3/metis/include \
-	  -DMETIS_LIBRARIES=$HOME/local/edison/intel17/parmetis-4.0.3/build/Linux-x86_64/libmetis/libmetis.a \
-	  -DPARMETIS_INCLUDES=$HOME/local/edison/intel17/parmetis-4.0.3/include \
-	  -DPARMETIS_LIBRARIES=$HOME/local/edison/intel17/parmetis-4.0.3/build/Linux-x86_64/libparmetis/libparmetis.a \
-	  -DSCOTCH_INCLUDES=$HOME/local/edison/intel17/scotch_6.0.4/include \
-	  -DSCOTCH_LIBRARIES="$HOME/local/edison/intel17/scotch_6.0.4/lib/libscotch.a;$HOME/local/edison/intel17/scotch_6.0.4/lib/libscotcherr.a;$HOME/local/edison/intel17/scotch_6.0.4/lib/libptscotch.a;$HOME/local/edison/intel17/scotch_6.0.4/lib/libptscotcherr.a"
+	  -DMETIS_INCLUDES=$HOME/local/parmetis-4.0.3/metis/include \
+	  -DMETIS_LIBRARIES=$HOME/local/parmetis-4.0.3/build/Linux-x86_64/libmetis/libmetis.a \
+	  -DPARMETIS_INCLUDES=$HOME/local/parmetis-4.0.3/include \
+	  -DPARMETIS_LIBRARIES=$HOME/local/parmetis-4.0.3/build/Linux-x86_64/libparmetis/libparmetis.a \
+	  -DSCOTCH_INCLUDES=$HOME/local/scotch_6.0.4/include \
+	  -DSCOTCH_LIBRARIES="$HOME/local/scotch_6.0.4/lib/libscotch.a;$HOME/local/scotch_6.0.4/lib/libscotcherr.a;$HOME/local/scotch_6.0.4/lib/libptscotch.a;$HOME/local/scotch_6.0.4/lib/libptscotcherr.a"
+fi
+
+if [[ $NERSC_HOST = "cori" ]]; then
+    found_host=true
+    #	  -DCMAKE_CXX_FLAGS="-DUSE_TASK_TIMER -DCOUNT_FLOPS" \
+    cmake ../STRUMPACK -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=../STRUMPACK-install \
+	  -DCMAKE_CXX_COMPILER=CC -DCMAKE_C_COMPILER=cc -DCMAKE_Fortran_COMPILER=ftn \
+	  -DCMAKE_EXE_LINKER_FLAGS="-dynamic" \
+	  -DMETIS_INCLUDES=$HOME/local/parmetis-4.0.3/metis/include \
+	  -DMETIS_LIBRARIES=$HOME/local/parmetis-4.0.3/build/Linux-x86_64/libmetis/libmetis.a \
+	  -DPARMETIS_INCLUDES=$HOME/local/parmetis-4.0.3/include \
+	  -DPARMETIS_LIBRARIES=$HOME/local/parmetis-4.0.3/build/Linux-x86_64/libparmetis/libparmetis.a \
+	  -DSCOTCH_INCLUDES=$HOME/local/scotch_6.0.4/include \
+	  -DSCOTCH_LIBRARIES="$HOME/local/scotch_6.0.4/lib/libscotch.a;$HOME/local/scotch_6.0.4/lib/libscotcherr.a;$HOME/local/scotch_6.0.4/lib/libptscotch.a;$HOME/local/scotch_6.0.4/lib/libptscotcherr.a"
 fi
 
 if [[ $(hostname -s) = "xps13" ]]; then
