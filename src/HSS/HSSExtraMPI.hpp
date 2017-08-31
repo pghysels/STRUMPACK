0c222f791e860e194c8769e6d608bc6cc770ee6c
pghysels <pghysels@lbl.gov>
2017-08-31 14:06:38 -0700

    Some formatting cleanups.

--- src/HSS/HSSExtraMPI.hpp
+++ src/HSS/HSSExtraMPI.hpp
@@ -7,23 +7,26 @@
 namespace strumpack {
   namespace HSS {
 
-    template<typename scalar_t> class WorkCompressMPI : public WorkCompressBase<scalar_t> {
+    template<typename scalar_t>
+    class WorkCompressMPI : public WorkCompressBase<scalar_t> {
     public:
       std::vector<WorkCompressMPI<scalar_t>> c;
       DistributedMatrix<scalar_t> Rr, Rc, Sr, Sc;
       DistributedMatrix<scalar_t> Qr, Qc;
       std::unique_ptr<WorkCompress<scalar_t>> w_seq;
       void split(const std::pair<std::size_t,std::size_t>& dim) {
-	if (c.empty()) {
-	  c.resize(2);
-	  c[0].offset = this->offset;
-	  c[1].offset = this->offset + dim;
-	  c[0].lvl = c[1].lvl = this->lvl + 1;
-	}
+        if (c.empty()) {
+          c.resize(2);
+          c[0].offset = this->offset;
+          c[1].offset = this->offset + dim;
+          c[0].lvl = c[1].lvl = this->lvl + 1;
+        }
       }
       void create_sequential() {
-	if (!w_seq) w_seq = std::unique_ptr<WorkCompress<scalar_t>>(new WorkCompress<scalar_t>());
-	w_seq->lvl = this->lvl;
+        if (!w_seq)
+          w_seq = std::unique_ptr<WorkCompress<scalar_t>>
+            (new WorkCompress<scalar_t>());
+        w_seq->lvl = this->lvl;
       }
     };
 
@@ -40,11 +43,11 @@ namespace strumpack {
     template<typename scalar_t> class DistSubLeaf {
     public:
       DistSubLeaf(int cols, const HSSMatrixBase<scalar_t>* H, int ctxt_loc)
-	: _cols(cols), _hss(H), _ctxt_loc(ctxt_loc) { allocate_block_row(); }
+        : _cols(cols), _hss(H), _ctxt_loc(ctxt_loc) { allocate_block_row(); }
       /** dist should be on the context of H */
       DistSubLeaf(int cols, const HSSMatrixBase<scalar_t>* H, int ctxt_loc,
-		  const DistributedMatrix<scalar_t>& dist)
-	: _cols(cols), _hss(H), _ctxt_loc(ctxt_loc) { to_block_row(dist); }
+                  const DistributedMatrix<scalar_t>& dist)
+        : _cols(cols), _hss(H), _ctxt_loc(ctxt_loc) { to_block_row(dist); }
       void from_block_row(DistributedMatrix<scalar_t>& dist) const
       { _hss->from_block_row(dist, sub, leaf, _ctxt_loc); }
       DistributedMatrix<scalar_t> leaf;
@@ -66,10 +69,11 @@ namespace strumpack {
       TreeLocalRanges() {}
       TreeLocalRanges(int P) : _ranges(5*P) {}
       void print() const {
-	std::cout << "ranges=[";
-	for (std::size_t p=0; p<_ranges.size()/5; p++)
-	  std::cout << rlo(p) << "," << rhi(p) << "/" << clo(p) << "," << chi(p) << "/" << leaf_procs(p) << " ";
-	std::cout << "];" << std::endl;
+        std::cout << "ranges=[";
+        for (std::size_t p=0; p<_ranges.size()/5; p++)
+          std::cout << rlo(p) << "," << rhi(p) << "/"
+                    << clo(p) << "," << chi(p) << "/" << leaf_procs(p) << " ";
+        std::cout << "];" << std::endl;
       }
       int rlo(int p) const { return _ranges[5*p+0]; }
       int rhi(int p) const { return _ranges[5*p+1]; }
@@ -89,8 +93,12 @@ namespace strumpack {
     template<typename scalar_t> class WorkFactorMPI {
     public:
       std::vector<WorkFactorMPI<scalar_t>> c;
-      DistributedMatrix<scalar_t> Dt;  // (U.cols x U.cols) \tilde(D)
-      DistributedMatrix<scalar_t> Vt1; // (U.cols x V.cols) bottom part of \tilde{V}
+
+      // (U.cols x U.cols) \tilde(D)
+      DistributedMatrix<scalar_t> Dt;
+
+      // (U.cols x V.cols) bottom part of \tilde{V}
+      DistributedMatrix<scalar_t> Vt1;
       std::unique_ptr<WorkFactor<scalar_t>> w_seq;
     };
 
@@ -101,27 +109,39 @@ namespace strumpack {
       const DistributedMatrix<scalar_t>& Vhat() const { return _Vt0; }
       DistributedMatrix<scalar_t>& Vhat() { return _Vt0; }
       std::size_t memory() {
-	std::size_t mem = sizeof(*this) + _L.memory() + _Vt0.memory() + _W1.memory()
-	  + _Q.memory() + _D.memory() + sizeof(int)*_piv.size();
-	for (auto& c : _ch) mem += c.memory();
-	return memory;
+        std::size_t mem = sizeof(*this) + _L.memory() + _Vt0.memory()
+          + _W1.memory() + _Q.memory() + _D.memory()
+          + sizeof(int)*_piv.size();
+        for (auto& c : _ch) mem += c.memory();
+        return memory;
       }
       std::size_t nonzeros() const {
-	std::size_t nnz = _L.nonzeros() + _Vt0.nonzeros() + _W1.nonzeros() + _Q.nonzeros() + _D.nonzeros();
-	for (auto& c : _ch) nnz += c.nonzeros();
-	return nnz;
+        std::size_t nnz = _L.nonzeros() + _Vt0.nonzeros() + _W1.nonzeros()
+          + _Q.nonzeros() + _D.nonzeros();
+        for (auto& c : _ch) nnz += c.nonzeros();
+        return nnz;
       }
     private:
       std::vector<HSSFactorsMPI<scalar_t>> _ch;
       std::unique_ptr<HSSFactors<scalar_t>> _factors_seq;
-      DistributedMatrix<scalar_t> _L;   // (U.rows-U.cols x U.rows-U.cols), empty at the root
-      DistributedMatrix<scalar_t> _Vt0; // (U.rows-U.cols x V.cols)
-                                        // at the root, _Vt0 stored Vhat
-      DistributedMatrix<scalar_t> _W1;  // (U.cols x U.rows) bottom part of W
-                                        // if (U.rows == U.cols) then W == I and is not stored!
-      DistributedMatrix<scalar_t> _Q;   // (U.rows x U.rows) Q from LQ(W0)
-                                        // if (U.rows == U.cols) then Q == I and is not stored!
-      DistributedMatrix<scalar_t> _D;   // (U.rows x U.rows) at the root holds LU(D), else empty
+
+      // (U.rows-U.cols x U.rows-U.cols), empty at the root
+      DistributedMatrix<scalar_t> _L;
+
+      // (U.rows-U.cols x V.cols)
+      // at the root, _Vt0 stored Vhat
+      DistributedMatrix<scalar_t> _Vt0;
+
+      // (U.cols x U.rows) bottom part of W
+      // if (U.rows == U.cols) then W == I and is not stored!
+      DistributedMatrix<scalar_t> _W1;
+
+      // (U.rows x U.rows) Q from LQ(W0)
+      // if (U.rows == U.cols) then Q == I and is not stored!
+      DistributedMatrix<scalar_t> _Q;
+
+      // (U.rows x U.rows) at the root holds LU(D), else empty
+      DistributedMatrix<scalar_t> _D;
       std::vector<int> _piv;            // hold permutation from LU(D) at root
     };
 
@@ -147,42 +167,47 @@ namespace strumpack {
       DistributedMatrix<scalar_t> y, z;
       std::vector<std::size_t> I, J, rl2g, cl2g, ycols, zcols;
       std::unique_ptr<WorkExtract<scalar_t>> w_seq;
-      void split_extraction_sets(const std::pair<std::size_t,std::size_t>& dim) {
-	if (c.empty()) {
-	  c.resize(2);
-	  c[0].I.reserve(I.size());
-	  c[1].I.reserve(I.size());
-	  for (auto i : I)
-	    if (i < dim.first) c[0].I.push_back(i);
-	    else c[1].I.push_back(i - dim.first);
-	  c[0].J.reserve(J.size());
-	  c[1].J.reserve(J.size());
-	  for (auto j : J)
-	    if (j < dim.second) c[0].J.push_back(j);
-	    else c[1].J.push_back(j - dim.second);
-	}
+      void split_extraction_sets
+      (const std::pair<std::size_t,std::size_t>& dim) {
+        if (c.empty()) {
+          c.resize(2);
+          c[0].I.reserve(I.size());
+          c[1].I.reserve(I.size());
+          for (auto i : I)
+            if (i < dim.first) c[0].I.push_back(i);
+            else c[1].I.push_back(i - dim.first);
+          c[0].J.reserve(J.size());
+          c[1].J.reserve(J.size());
+          for (auto j : J)
+            if (j < dim.second) c[0].J.push_back(j);
+            else c[1].J.push_back(j - dim.second);
+        }
       }
       void communicate_child_ycols(MPI_Comm comm, int rch1) {
-	auto rch0 = 0;
-	std::size_t c0ycols = c[0].ycols.size();
-	MPI_Bcast(&c0ycols, 1, mpi_type<std::size_t>(), rch0, comm);
-	std::size_t c1ycols = c[1].ycols.size();
-	MPI_Bcast(&c1ycols, 1, mpi_type<std::size_t>(), rch1, comm);
-	c[0].ycols.resize(c0ycols);
-	c[1].ycols.resize(c1ycols);
-	MPI_Bcast(c[0].ycols.data(), c0ycols, mpi_type<std::size_t>(), rch0, comm);
-	MPI_Bcast(c[1].ycols.data(), c1ycols, mpi_type<std::size_t>(), rch1, comm);
+        auto rch0 = 0;
+        std::size_t c0ycols = c[0].ycols.size();
+        MPI_Bcast(&c0ycols, 1, mpi_type<std::size_t>(), rch0, comm);
+        std::size_t c1ycols = c[1].ycols.size();
+        MPI_Bcast(&c1ycols, 1, mpi_type<std::size_t>(), rch1, comm);
+        c[0].ycols.resize(c0ycols);
+        c[1].ycols.resize(c1ycols);
+        MPI_Bcast(c[0].ycols.data(), c0ycols, mpi_type<std::size_t>(),
+                  rch0, comm);
+        MPI_Bcast(c[1].ycols.data(), c1ycols, mpi_type<std::size_t>(),
+                  rch1, comm);
       }
       void combine_child_ycols() {
-	auto c0ycols = c[0].ycols.size();
-	auto c1ycols = c[1].ycols.size();
-	ycols.resize(c0ycols + c1ycols);
-	std::copy(c[0].ycols.begin(), c[0].ycols.end(), ycols.begin());
-	std::copy(c[1].ycols.begin(), c[1].ycols.end(), ycols.begin()+c0ycols);
+        auto c0ycols = c[0].ycols.size();
+        auto c1ycols = c[1].ycols.size();
+        ycols.resize(c0ycols + c1ycols);
+        std::copy(c[0].ycols.begin(), c[0].ycols.end(), ycols.begin());
+        std::copy(c[1].ycols.begin(), c[1].ycols.end(),
+                  ycols.begin()+c0ycols);
       }
     };
 
-    template<typename scalar_t> void create_triplet_mpi_type(MPI_Datatype* triplet_type) {
+    template<typename scalar_t> void
+    create_triplet_mpi_type(MPI_Datatype* triplet_type) {
       MPI_Type_contiguous(sizeof(Triplet<scalar_t>), MPI_BYTE, triplet_type);
       MPI_Type_commit(triplet_type);
     }
--- src/MetisReordering.hpp
+++ src/MetisReordering.hpp
@@ -1,25 +1,29 @@
 /*
- * STRUMPACK -- STRUctured Matrices PACKage, Copyright (c) 2014, The Regents of
- * the University of California, through Lawrence Berkeley National Laboratory
- * (subject to receipt of any required approvals from the U.S. Dept. of Energy).
- * All rights reserved.
+ * STRUMPACK -- STRUctured Matrices PACKage, Copyright (c) 2014, The
+ * Regents of the University of California, through Lawrence Berkeley
+ * National Laboratory (subject to receipt of any required approvals
+ * from the U.S. Dept. of Energy).  All rights reserved.
  *
- * If you have questions about your rights to use or distribute this software,
- * please contact Berkeley Lab's Technology Transfer Department at TTD@lbl.gov.
+ * If you have questions about your rights to use or distribute this
+ * software, please contact Berkeley Lab's Technology Transfer
+ * Department at TTD@lbl.gov.
  *
- * NOTICE. This software is owned by the U.S. Department of Energy. As such, the
- * U.S. Government has been granted for itself and others acting on its behalf a
- * paid-up, nonexclusive, irrevocable, worldwide license in the Software to
- * reproduce, prepare derivative works, and perform publicly and display publicly.
- * Beginning five (5) years after the date permission to assert copyright is
- * obtained from the U.S. Department of Energy, and subject to any subsequent five
- * (5) year renewals, the U.S. Government is granted for itself and others acting
- * on its behalf a paid-up, nonexclusive, irrevocable, worldwide license in the
- * Software to reproduce, prepare derivative works, distribute copies to the
- * public, perform publicly and display publicly, and to permit others to do so.
+ * NOTICE. This software is owned by the U.S. Department of Energy. As
+ * such, the U.S. Government has been granted for itself and others
+ * acting on its behalf a paid-up, nonexclusive, irrevocable,
+ * worldwide license in the Software to reproduce, prepare derivative
+ * works, and perform publicly and display publicly.  Beginning five
+ * (5) years after the date permission to assert copyright is obtained
+ * from the U.S. Department of Energy, and subject to any subsequent
+ * five (5) year renewals, the U.S. Government is granted for itself
+ * and others acting on its behalf a paid-up, nonexclusive,
+ * irrevocable, worldwide license in the Software to reproduce,
+ * prepare derivative works, distribute copies to the public, perform
+ * publicly and display publicly, and to permit others to do so.
  *
  * Developers: Pieter Ghysels, Francois-Henry Rouet, Xiaoye S. Li.
- *             (Lawrence Berkeley National Lab, Computational Research Division).
+ *             (Lawrence Berkeley National Lab, Computational Research
+ *             Division).
  *
  */
 #ifndef METIS_REORDERING_HPP
@@ -39,15 +43,19 @@ namespace strumpack {
    idx_t* options, integer_t* perm, integer_t* iperm, idx_t* sizes) {
     auto order = new idx_t[2*n];
     auto iorder = order + n;
-    int ierr = METIS_NodeNDP(n, xadj, adjncy, vwgt, seps, options, order, iorder, sizes);
+    int ierr = METIS_NodeNDP(n, xadj, adjncy, vwgt, seps,
+                             options, order, iorder, sizes);
     std::copy(order, order+n, perm);
     std::copy(iorder, iorder+n, iperm);
     delete[] order;
     return ierr;
   }
-  template<> inline int WRAPPER_METIS_NodeNDP(idx_t n, idx_t* xadj, idx_t* adjncy, idx_t* vwgt, idx_t seps,
-					      idx_t* options, idx_t* perm, idx_t* iperm, idx_t* sizes) {
-    return METIS_NodeNDP(n, xadj, adjncy, vwgt, seps, options, perm, iperm, sizes);
+  template<> inline int
+  WRAPPER_METIS_NodeNDP(idx_t n, idx_t* xadj, idx_t* adjncy, idx_t* vwgt,
+                        idx_t seps, idx_t* options,
+                        idx_t* perm, idx_t* iperm, idx_t* sizes) {
+    return METIS_NodeNDP(n, xadj, adjncy, vwgt, seps, options,
+                         perm, iperm, sizes);
   }
 
   template<typename integer_t> inline int WRAPPER_METIS_NodeND
@@ -62,35 +70,42 @@ namespace strumpack {
     delete[] order;
     return ierr;
   }
-  template<> inline int WRAPPER_METIS_NodeND(idx_t n, idx_t* xadj, idx_t* adjncy, idx_t* vwgt,
-					     idx_t* options, idx_t* perm, idx_t* iperm) {
+  template<> inline int
+  WRAPPER_METIS_NodeND(idx_t n, idx_t* xadj, idx_t* adjncy, idx_t* vwgt,
+                       idx_t* options, idx_t* perm, idx_t* iperm) {
     return METIS_NodeND(&n, xadj, adjncy, vwgt, options, perm, iperm);
   }
 
   // this is used in separator reordering
   template<typename integer_t> inline int WRAPPER_METIS_PartGraphRecursive
-  (idx_t* nvtxs, idx_t* ncon, std::vector<integer_t>& sep_csr_ptr, std::vector<integer_t>& sep_csr_ind,
-   idx_t* two, idx_t* edge_cut, idx_t* partitioning) {
+  (idx_t* nvtxs, idx_t* ncon, std::vector<integer_t>& sep_csr_ptr,
+   std::vector<integer_t>& sep_csr_ind, idx_t* two,
+   idx_t* edge_cut, idx_t* partitioning) {
     auto ptr = new idx_t[sep_csr_ptr.size()+sep_csr_ind.size()];
     auto ind = ptr + sep_csr_ptr.size();
     for (size_t i=0; i<sep_csr_ptr.size(); i++) ptr[i] = sep_csr_ptr[i];
     for (size_t i=0; i<sep_csr_ind.size(); i++) ind[i] = sep_csr_ind[i];
-    int ierr = METIS_PartGraphRecursive(nvtxs, ncon, ptr, ind, NULL, NULL, NULL,
-					two, NULL, NULL, NULL, edge_cut, partitioning);
+    int ierr = METIS_PartGraphRecursive
+      (nvtxs, ncon, ptr, ind, NULL, NULL, NULL,
+       two, NULL, NULL, NULL, edge_cut, partitioning);
     delete[] ptr;
     return ierr;
   }
   template<> inline int WRAPPER_METIS_PartGraphRecursive
-  (idx_t* nvtxs, idx_t* ncon, std::vector<idx_t>& sep_csr_ptr, std::vector<idx_t>& sep_csr_ind,
-   idx_t* two, idx_t* edge_cut, idx_t* partitioning) {
-    return METIS_PartGraphRecursive(nvtxs, ncon, sep_csr_ptr.data(), sep_csr_ind.data(),
-				    NULL, NULL, NULL, two, NULL, NULL, NULL, edge_cut, partitioning);
+  (idx_t* nvtxs, idx_t* ncon, std::vector<idx_t>& sep_csr_ptr,
+   std::vector<idx_t>& sep_csr_ind, idx_t* two,
+   idx_t* edge_cut, idx_t* partitioning) {
+    return METIS_PartGraphRecursive
+      (nvtxs, ncon, sep_csr_ptr.data(), sep_csr_ind.data(),
+       NULL, NULL, NULL, two, NULL, NULL, NULL, edge_cut, partitioning);
   }
 
 
   template<typename integer_t> std::unique_ptr<SeparatorTree<integer_t>>
-  sep_tree_from_metis_sizes(integer_t nodes, integer_t separators, idx_t* sizes) {
-    std::unique_ptr<SeparatorTree<integer_t>> sep_tree(new SeparatorTree<integer_t>(nodes));
+  sep_tree_from_metis_sizes(integer_t nodes, integer_t
+                            separators, idx_t* sizes) {
+    std::unique_ptr<SeparatorTree<integer_t>>
+      sep_tree(new SeparatorTree<integer_t>(nodes));
 
     // Generates the parent positions of all the nodes in a
     // subtree, except for its root, and returns the number
@@ -104,12 +119,12 @@ namespace strumpack {
       parent = [&](integer_t pos, integer_t v) -> integer_t {
       integer_t s0, s1;
       if (v < separators) {
-  	integer_t root;
-  	s0 = parent(pos, 2 * v + 2);
-  	s1 = parent(pos + s0, 2 * v + 1);
-  	root = pos + s0 + s1;
-  	sep_tree->pa()[pos + s0 - 1] = root;
-  	sep_tree->pa()[pos + s0 + s1 - 1] = root;
+        integer_t root;
+        s0 = parent(pos, 2 * v + 2);
+        s1 = parent(pos + s0, 2 * v + 1);
+        root = pos + s0 + s1;
+        sep_tree->pa()[pos + s0 - 1] = root;
+        sep_tree->pa()[pos + s0 + s1 - 1] = root;
       } else std::tie(s0, s1) = std::make_tuple(0, 0);
       return s0 + s1 + 1;
     };
@@ -123,16 +138,16 @@ namespace strumpack {
       children = [&](integer_t pos, integer_t v) -> integer_t {
       integer_t s0, s1;
       if (v < separators) {
-	integer_t root;
-	s0 = children(pos, 2 * v + 2);
-	s1 = children(pos + s0, 2 * v + 1);
-	root = pos + s0 + s1;
-	sep_tree->lch()[root] = pos + s0 - 1;
-	sep_tree->rch()[root] = pos + s0 + s1 - 1;
+        integer_t root;
+        s0 = children(pos, 2 * v + 2);
+        s1 = children(pos + s0, 2 * v + 1);
+        root = pos + s0 + s1;
+        sep_tree->lch()[root] = pos + s0 - 1;
+        sep_tree->rch()[root] = pos + s0 + s1 - 1;
       } else {
-	std::tie(s0, s1) = std::make_tuple(0, 0);
-	sep_tree->lch()[pos] = -1;
-	sep_tree->rch()[pos] = -1;
+        std::tie(s0, s1) = std::make_tuple(0, 0);
+        sep_tree->lch()[pos] = -1;
+        sep_tree->rch()[pos] = -1;
       }
       return s0 + s1 + 1;
     };
@@ -148,11 +163,11 @@ namespace strumpack {
       offset = [&](integer_t pos, integer_t offs, integer_t v) -> ss {
       integer_t s0, s1, q0, q1;
       if (v < separators) {
-	std::tie(s0, q0) = offset(pos, offs, 2 * v + 2);
-	std::tie(s1, q1) = offset(pos + s0, offs + q0, 2 * v + 1);
+        std::tie(s0, q0) = offset(pos, offs, 2 * v + 2);
+        std::tie(s1, q1) = offset(pos + s0, offs + q0, 2 * v + 1);
       } else {
-	std::tie(s0, s1) = std::make_pair(0, 0);
-	std::tie(q0, q1) = std::make_pair(0, 0);
+        std::tie(s0, s1) = std::make_pair(0, 0);
+        std::tie(q0, q1) = std::make_pair(0, 0);
       }
       sep_tree->sizes()[pos + s0 + s1] = offs + q0 + q1;
       return std::make_pair(s0 + s1 + 1, q0 + q1 + sizes[nodes - (v + 1)]);
@@ -164,9 +179,11 @@ namespace strumpack {
   }
 
   // TODO throw an exception
-  template<typename scalar_t,typename integer_t> std::unique_ptr<SeparatorTree<integer_t>>
+  template<typename scalar_t,typename integer_t>
+  std::unique_ptr<SeparatorTree<integer_t>>
   metis_nested_dissection(CompressedSparseMatrix<scalar_t,integer_t>* A,
-			  integer_t* perm, integer_t* iperm, const SPOptions<scalar_t>& opts) {
+                          integer_t* perm, integer_t* iperm,
+                          const SPOptions<scalar_t>& opts) {
     auto n = A->size();
     auto ptr = A->get_ptr();
     auto ind = A->get_ind();
@@ -176,31 +193,36 @@ namespace strumpack {
     for (integer_t j=0; j<n; j++) {
       xadj[j] = e;
       for (integer_t t=ptr[j]; t<ptr[j+1]; t++)
-	if (ind[t] != j) adjncy[e++] = ind[t];
+        if (ind[t] != j) adjncy[e++] = ind[t];
     }
     xadj[n] = e;
-    if (e==0) if (mpi_root()) std::cerr << "# WARNING: matrix seems to be diagonal!" << std::endl;
+    if (e==0)
+      if (mpi_root())
+        std::cerr << "# WARNING: matrix seems to be diagonal!" << std::endl;
     int ierr;
     std::unique_ptr<SeparatorTree<integer_t>> sep_tree;
     if (opts.use_METIS_NodeNDP()) {
-      integer_t nodes = std::max(integer_t(3), ( n / opts.nd_param() ) / 2 * 2 + 1);
+      integer_t nodes =
+        std::max(integer_t(3), ( n / opts.nd_param() ) / 2 * 2 + 1);
       integer_t separators = nodes / 2;
       auto sizes = new idx_t[nodes + 1];
-      ierr = WRAPPER_METIS_NodeNDP(n, xadj, adjncy, NULL, separators + 1, NULL, iperm, perm, sizes);
+      ierr = WRAPPER_METIS_NodeNDP(n, xadj, adjncy, NULL, separators + 1,
+                                   NULL, iperm, perm, sizes);
       delete[] xadj;
       if (opts.use_MUMPS_SYMQAMD())
-	sep_tree = aggressive_amalgamation(A, perm, iperm, opts);
+        sep_tree = aggressive_amalgamation(A, perm, iperm, opts);
       else sep_tree = sep_tree_from_metis_sizes(nodes, separators, sizes);
       delete[] sizes;
     } else {
       ierr = WRAPPER_METIS_NodeND(n, xadj, adjncy, NULL, NULL, iperm, perm);
       delete[] xadj;
       if (opts.use_MUMPS_SYMQAMD())
-	sep_tree = aggressive_amalgamation(A, perm, iperm, opts);
+        sep_tree = aggressive_amalgamation(A, perm, iperm, opts);
       else sep_tree = build_sep_tree_from_perm(n, ptr, ind, perm, iperm);
     }
     if (ierr != METIS_OK) {
-      std::cerr << "# ERROR: Metis nested dissection reordering failed with error code " << ierr << std::endl;
+      std::cerr << "# ERROR: Metis nested dissection reordering failed"
+                << " with error code " << ierr << std::endl;
       // TODO throw an exception
     }
     return sep_tree;
