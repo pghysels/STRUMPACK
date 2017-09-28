3f7155a7abd5c2ab628050b070ad35a7489e91f2
Pieter Ghysels <pghysels@lbl.gov>
2017-09-27 23:07:39 -0700

    Formatting.

--- test/test_sparse_mpi.cpp
+++ test/test_sparse_mpi.cpp
@@ -1,29 +1,35 @@
 /*
- * STRUMPACK -- STRUctured Matrices PACKage, Copyright (c) 2014, The Regents of
- * the University of California, through Lawrence Berkeley National Laboratory
- * (subject to receipt of any required approvals from the U.S. Dept. of Energy).
- * All rights reserved.
+ * STRUMPACK -- STRUctured Matrices PACKage, Copyright (c) 2014, The
+ * Regents of the University of California, through Lawrence Berkeley
+ * National Laboratory (subject to receipt of any required approvals
+ * from the U.S. Dept. of Energy).  All rights reserved.
  *
- * If you have questions about your rights to use or distribute this software,
- * please contact Berkeley Lab's Technology Transfer Department at TTD@lbl.gov.
+ * If you have questions about your rights to use or distribute this
+ * software, please contact Berkeley Lab's Technology Transfer
+ * Department at TTD@lbl.gov.
  *
- * NOTICE. This software is owned by the U.S. Department of Energy. As such, the
- * U.S. Government has been granted for itself and others acting on its behalf a
- * paid-up, nonexclusive, irrevocable, worldwide license in the Software to
- * reproduce, prepare derivative works, and perform publicly and display publicly.
- * Beginning five (5) years after the date permission to assert copyright is
- * obtained from the U.S. Department of Energy, and subject to any subsequent five
- * (5) year renewals, the U.S. Government is granted for itself and others acting
- * on its behalf a paid-up, nonexclusive, irrevocable, worldwide license in the
- * Software to reproduce, prepare derivative works, distribute copies to the
- * public, perform publicly and display publicly, and to permit others to do so.
+ * NOTICE. This software is owned by the U.S. Department of Energy. As
+ * such, the U.S. Government has been granted for itself and others
+ * acting on its behalf a paid-up, nonexclusive, irrevocable,
+ * worldwide license in the Software to reproduce, prepare derivative
+ * works, and perform publicly and display publicly.  Beginning five
+ * (5) years after the date permission to assert copyright is obtained
+ * from the U.S. Department of Energy, and subject to any subsequent
+ * five (5) year renewals, the U.S. Government is granted for itself
+ * and others acting on its behalf a paid-up, nonexclusive,
+ * irrevocable, worldwide license in the Software to reproduce,
+ * prepare derivative works, distribute copies to the public, perform
+ * publicly and display publicly, and to permit others to do so.
  *
  * Developers: Pieter Ghysels, Francois-Henry Rouet, Xiaoye S. Li,.
- *             (Lawrence Berkeley National Lab, Computational Research Division).
+ *             (Lawrence Berkeley National Lab, Computational Research
+ *             Division).
  *
  */
 #include <iostream>
 #include <sstream>
+using namespace std;
+
 #include <getopt.h>
 
 #define STRUMPACK_PBLAS_BLOCKSIZE 3
@@ -37,7 +43,7 @@
 using namespace strumpack;
 
 void abort_MPI(MPI_Comm *c, int *error, ...) {
-  std::cout << "rank = " << mpi_rank() << " ABORTING!!!!!" << std::endl;
+  cout << "rank = " << mpi_rank() << " ABORTING!!!!!" << endl;
   abort();
 }
 
@@ -53,28 +59,37 @@ test(int argc, char* argv[], CSRMatrixMPI<scalar,integer>* Adist) {
 
   auto N = Adist->size();
   auto n_local = Adist->local_rows();
-  std::vector<scalar> b(n_local), x(n_local), x_exact(n_local, scalar(1.)/std::sqrt(N));
+  vector<scalar> b(n_local), x(n_local),
+    x_exact(n_local, scalar(1.)/sqrt(N));
   Adist->omp_spmv(x_exact.data(), b.data());
 
-  spss.set_distributed_csr_matrix(Adist->local_rows(), Adist->get_ptr(), Adist->get_ind(),
-				  Adist->get_val(), Adist->get_dist().data(), Adist->has_symmetric_sparsity());
+  spss.set_distributed_csr_matrix
+    (Adist->local_rows(), Adist->get_ptr(), Adist->get_ind(),
+     Adist->get_val(), Adist->get_dist().data(),
+     Adist->has_symmetric_sparsity());
   if (spss.reorder() != ReturnCode::SUCCESS) {
-    if (!rank) std::cout << "problem with reordering of the matrix." << std::endl;
+    if (!rank)
+      cout << "problem with reordering of the matrix." << endl;
     MPI_Abort(MPI_COMM_WORLD, 1);
   }
   if (spss.factor() != ReturnCode::SUCCESS) {
-    if (!rank) std::cout << "problem during factorization of the matrix." << std::endl;
+    if (!rank)
+      cout << "problem during factorization of the matrix." << endl;
     MPI_Abort(MPI_COMM_WORLD, 1);
   }
   spss.solve(b.data(), x.data());
 
   auto scaled_res = Adist->max_scaled_residual(x.data(), b.data());
-  if (!rank) std::cout << "# COMPONENTWISE SCALED RESIDUAL = " << scaled_res << std::endl;
+  if (!rank)
+    cout << "# COMPONENTWISE SCALED RESIDUAL = "
+         << scaled_res << endl;
 
   blas::axpy(n_local, scalar(-1.), x_exact.data(), 1, x.data(), 1);
   auto nrm_error = nrm2_omp_mpi(n_local, x.data(), 1, MPI_COMM_WORLD);
   auto nrm_x_exact = nrm2_omp_mpi(n_local, x_exact.data(), 1, MPI_COMM_WORLD);
-  if (!rank) std::cout << "# RELATIVE ERROR = " << (nrm_error/nrm_x_exact) << std::endl;
+  if (!rank)
+    cout << "# RELATIVE ERROR = "
+         << (nrm_error/nrm_x_exact) << endl;
 
   if (scaled_res > ERROR_TOLERANCE*spss.options().rel_tol())
     MPI_Abort(MPI_COMM_WORLD, 1);
@@ -87,30 +102,34 @@ int main(int argc, char* argv[]) {
   //MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &thread_level);
   int rank = mpi_rank();
   if (!rank) {
-    std::cout << "# Running with:\n# ";
+    cout << "# Running with:\n# ";
 #if defined(_OPENMP)
-    std::cout << "OMP_NUM_THREADS=" << omp_get_max_threads() << " mpirun -n " << mpi_nprocs() << " ";
+    cout << "OMP_NUM_THREADS=" << omp_get_max_threads()
+         << " mpirun -n " << mpi_nprocs() << " ";
 #else
-    std::cout << "mpirun -n " << mpi_nprocs() << " ";
+    cout << "mpirun -n " << mpi_nprocs() << " ";
 #endif
-    for (int i=0; i<argc; i++) std::cout << argv[i] << " ";
-    std::cout << std::endl;
+    for (int i=0; i<argc; i++) cout << argv[i] << " ";
+    cout << endl;
   }
   // if (thread_level != MPI_THREAD_FUNNELED && rank == 0)
-  //   std::cout << "MPI implementation does not support MPI_THREAD_FUNNELED" << std::endl;
+  //   cout << "MPI implementation does not support MPI_THREAD_FUNNELED"
+  //             << endl;
   if (thread_level != MPI_THREAD_MULTIPLE && rank == 0)
-    std::cout << "MPI implementation does not support MPI_THREAD_MULTIPLE, "
-      "which might be needed for pt-scotch!" << std::endl;
+    cout << "MPI implementation does not support MPI_THREAD_MULTIPLE, "
+      "which might be needed for pt-scotch!" << endl;
 
   if (argc < 3) {
     if (!rank)
-      std::cout << "Solve a linear system with a matrix given in matrix market format" << std::endl
-		<< "using the MPI fully distributed C++ STRUMPACK interface."
-		<< std::endl << std::endl
-		<< "Usage: \n\tmpirun -n 4 ./testMMdoubleMPIDist m pde900.mtx" << std::endl
-		<< "or\n\tmpirun -n 4 ./testMMdoubleMPIDist b pde900.bin" << std::endl
-		<< "Specify the matrix input file with 'm filename' if the matrix is in matrix-market format," << std::endl
-		<< "or with 'b filename' if the matrix is in binary." << std::endl;
+      cout
+        << "Solve a linear system with a matrix given"
+        << " in matrix market format\n"
+        << "using the MPI fully distributed C++ STRUMPACK interface.\n\n"
+        << "Usage: \n\tmpirun -n 4 ./testMMdoubleMPIDist m pde900.mtx\n"
+        << "or\n\tmpirun -n 4 ./testMMdoubleMPIDist b pde900.bin\n"
+        << "Specify the matrix input file with 'm filename'"
+        << " if the matrix is in matrix-market format,\n"
+        << "or with 'b filename' if the matrix is in binary." << endl;
     MPI_Abort(MPI_COMM_WORLD, 1);
   }
 
@@ -119,15 +138,16 @@ int main(int argc, char* argv[]) {
   MPI_Comm_set_errhandler(MPI_COMM_WORLD, eh);
 
   bool binary_input = false;
-  std::string format(argv[1]);
+  string format(argv[1]);
   if (format.compare("b") == 0) binary_input = true;
   else if (format.compare("m") == 0) binary_input = false;
-  else std::cerr << "Error format is eiter b (binary) or m (matrix market)." << std::endl;
+  else cerr << "Error format is eiter b (binary) or m (matrix market)."
+            << endl;
 
-  std::string f(argv[2]);
+  string f(argv[2]);
 
   CSRMatrix<double,int>* A = NULL;
-  CSRMatrix<std::complex<double>,int>* A_c = NULL;
+  CSRMatrix<complex<double>,int>* A_c = NULL;
   bool is_complex = false;
 
   if (rank == 0) {
@@ -137,27 +157,27 @@ int main(int argc, char* argv[]) {
     else ierr = A->read_matrix_market(f);
     if (!ierr) {
       is_complex = false;
-      std::vector<int> perm;
-      std::vector<double> Dr;
-      std::vector<double> Dc;
+      vector<int> perm;
+      vector<double> Dr;
+      vector<double> Dc;
       ierr = A->permute_and_scale(5, perm, Dr, Dc);
-      if (ierr) std::cerr << "MC64 reordering failed!" << std::endl;
+      if (ierr) cerr << "MC64 reordering failed!" << endl;
       A->symmetrize_sparsity();
     } else {
       delete A;
       is_complex = true;
-      A_c = new CSRMatrix<std::complex<double>,int>();
+      A_c = new CSRMatrix<complex<double>,int>();
       if (binary_input) ierr = A_c->read_binary(f);
       else ierr = A_c->read_matrix_market(f);
       if (ierr) {
-        std::cerr << "Could not read matrix from file." << std::endl;
+        cerr << "Could not read matrix from file." << endl;
         MPI_Abort(MPI_COMM_WORLD, 1);
       }
-      std::vector<int> perm;
-      std::vector<std::complex<double>> Dr;
-      std::vector<std::complex<double>> Dc;
+      vector<int> perm;
+      vector<complex<double>> Dr;
+      vector<complex<double>> Dc;
       ierr = A_c->permute_and_scale(5, perm, Dr, Dc);
-      if (ierr) std::cerr << "MC64 reordering failed!" << std::endl;
+      if (ierr) cerr << "MC64 reordering failed!" << endl;
       A_c->symmetrize_sparsity();
     }
   }
@@ -170,7 +190,8 @@ int main(int argc, char* argv[]) {
     ierr = test(argc, argv, Adist);
     delete Adist;
   } else {
-    auto Adist_c = new CSRMatrixMPI<std::complex<double>,int>(A_c, MPI_COMM_WORLD, true);
+    auto Adist_c = new CSRMatrixMPI<complex<double>,int>
+      (A_c, MPI_COMM_WORLD, true);
     delete A_c;
     ierr = test(argc, argv, Adist_c);
     delete Adist_c;
--- test/test_sparse_seq.cpp
+++ test/test_sparse_seq.cpp
@@ -1,28 +1,34 @@
 /*
- * STRUMPACK -- STRUctured Matrices PACKage, Copyright (c) 2014, The Regents of
- * the University of California, through Lawrence Berkeley National Laboratory
- * (subject to receipt of any required approvals from the U.S. Dept. of Energy).
- * All rights reserved.
+ * STRUMPACK -- STRUctured Matrices PACKage, Copyright (c) 2014, The
+ * Regents of the University of California, through Lawrence Berkeley
+ * National Laboratory (subject to receipt of any required approvals
+ * from the U.S. Dept. of Energy).  All rights reserved.
  *
- * If you have questions about your rights to use or distribute this software,
- * please contact Berkeley Lab's Technology Transfer Department at TTD@lbl.gov.
+ * If you have questions about your rights to use or distribute this
+ * software, please contact Berkeley Lab's Technology Transfer
+ * Department at TTD@lbl.gov.
  *
- * NOTICE. This software is owned by the U.S. Department of Energy. As such, the
- * U.S. Government has been granted for itself and others acting on its behalf a
- * paid-up, nonexclusive, irrevocable, worldwide license in the Software to
- * reproduce, prepare derivative works, and perform publicly and display publicly.
- * Beginning five (5) years after the date permission to assert copyright is
- * obtained from the U.S. Department of Energy, and subject to any subsequent five
- * (5) year renewals, the U.S. Government is granted for itself and others acting
- * on its behalf a paid-up, nonexclusive, irrevocable, worldwide license in the
- * Software to reproduce, prepare derivative works, distribute copies to the
- * public, perform publicly and display publicly, and to permit others to do so.
+ * NOTICE. This software is owned by the U.S. Department of Energy. As
+ * such, the U.S. Government has been granted for itself and others
+ * acting on its behalf a paid-up, nonexclusive, irrevocable,
+ * worldwide license in the Software to reproduce, prepare derivative
+ * works, and perform publicly and display publicly.  Beginning five
+ * (5) years after the date permission to assert copyright is obtained
+ * from the U.S. Department of Energy, and subject to any subsequent
+ * five (5) year renewals, the U.S. Government is granted for itself
+ * and others acting on its behalf a paid-up, nonexclusive,
+ * irrevocable, worldwide license in the Software to reproduce,
+ * prepare derivative works, distribute copies to the public, perform
+ * publicly and display publicly, and to permit others to do so.
  *
  * Developers: Pieter Ghysels, Francois-Henry Rouet, Xiaoye S. Li.
- *             (Lawrence Berkeley National Lab, Computational Research Division).
+ *             (Lawrence Berkeley National Lab, Computational Research
+ *             Division).
  *
  */
 #include <iostream>
+using namespace std;
+
 #include "StrumpackSparseSolver.hpp"
 #include "CSRMatrix.hpp"
 #include "CSCMatrix.hpp"
@@ -40,27 +46,28 @@ test(int argc, char* argv[], CSRMatrix<scalar_t,integer_t>& A) {
   TaskTimer::t_begin = GET_TIME_NOW();
 
   int N = A.size();
-  std::vector<scalar_t> b(N), x(N), x_exact(N, scalar_t(1.)/std::sqrt(N));
+  vector<scalar_t> b(N), x(N), x_exact(N, scalar_t(1.)/sqrt(N));
   A.omp_spmv(x_exact.data(), b.data());
 
   spss.set_matrix(A);
   if (spss.reorder() != ReturnCode::SUCCESS) {
-    std::cout << "problem with reordering of the matrix." << std::endl;
+    cout << "problem with reordering of the matrix." << endl;
     return 1;
   }
   if (spss.factor() != ReturnCode::SUCCESS) {
-    std::cout << "problem during factorization of the matrix." << std::endl;
+    cout << "problem during factorization of the matrix." << endl;
     return 1;
   }
   spss.solve(b.data(), x.data());
 
   auto comp_scal_res = A.max_scaled_residual(x.data(), b.data());
-  std::cout << "# COMPONENTWISE SCALED RESIDUAL = " << comp_scal_res << std::endl;
+  cout << "# COMPONENTWISE SCALED RESIDUAL = "
+       << comp_scal_res << endl;
 
   blas::axpy(N, scalar_t(-1.), x_exact.data(), 1, x.data(), 1);
   auto nrm_error = blas::nrm2(N, x.data(), 1);
   auto nrm_x_exact = blas::nrm2(N, x_exact.data(), 1);
-  std::cout << "# RELATIVE ERROR = " << (nrm_error/nrm_x_exact) << std::endl;
+  cout << "# RELATIVE ERROR = " << (nrm_error/nrm_x_exact) << endl;
 
   if (comp_scal_res > ERROR_TOLERANCE*spss.options().rel_tol()) return 1;
   else return 0;
@@ -68,27 +75,27 @@ test(int argc, char* argv[], CSRMatrix<scalar_t,integer_t>& A) {
 
 int main(int argc, char* argv[]) {
   if (argc < 2) {
-    std::cout << "Solve a linear system with a matrix given in matrix market format" << std::endl
-	      << "using the sequential/multithreaded C++ STRUMPACK interface."
-	      << std::endl << std::endl
-	      << "Usage: \n\t./testMMdouble pde900.mtx" << std::endl;
+    cout
+      << "Solve a linear system with a matrix given in matrix market format\n"
+      << "using the sequential/multithreaded C++ STRUMPACK interface.\n\n"
+      << "Usage: \n\t./testMMdouble pde900.mtx" << endl;
     return 1;
   }
-  std::string f(argv[1]);
+  string f(argv[1]);
 
-  std::cout << "# Running with:\n# ";
+  cout << "# Running with:\n# ";
 #if defined(_OPENMP)
-  std::cout << "OMP_NUM_THREADS=" << omp_get_max_threads() << " ";
+  cout << "OMP_NUM_THREADS=" << omp_get_max_threads() << " ";
 #endif
-  for (int i=0; i<argc; i++) std::cout << argv[i] << " ";
-  std::cout << std::endl;
+  for (int i=0; i<argc; i++) cout << argv[i] << " ";
+  cout << endl;
 
   CSRMatrix<double,int> A;
   if (A.read_matrix_market(f) == 0)
     return test<double,int>(argc, argv, A);
   else {
-    CSRMatrix<std::complex<double>,int> A;
+    CSRMatrix<complex<double>,int> A;
     A.read_matrix_market(f);
-    return test<std::complex<double>,int>(argc, argv, A);
+    return test<complex<double>,int>(argc, argv, A);
   }
 }
