06a703ec784057c7151706984dd4fc0e53665c12
Gustavo Chávez <gchavez2@gmail.com>
2018-09-14 11:29:26 -0700

    Tests with increasing accuracy and dd

--- runSUSY.sh
+++ runSUSY.sh
@@ -1,20 +1,33 @@
 #!/usr/bin/env bash
 
-DATA="/Users/gichavez/Documents/mats/SUSY1K_latest/susy_1Kn"
 DATA="/Users/gichavez/Documents/mats/SUSY1/susy_train_10K_test_1K/susy_10Kn"
-
 EXEC="/Users/gichavez/Documents/Github/code_strann/build/examples/KernelRegression_ann"
 
-# Only valid and test for 10k
-# DATA="/Users/gichavez/Documents/Github/mats/SUSY3/susy_N"
-# DATA="/Users/gichavez/Documents/Github/mats/SUSY3/susy_N"
-
+# Kernel type, Gaussian=1, Laplacian=2
 kernel=1
+
+# Data dimension
 dim=8
+
+# Gaussian parameters h and lambda:
 hval=1
 lval=4
 
+
 RTOL=1e-1
+DD=128
 time ${EXEC} ${DATA} ${dim} ${hval} ${lval} ${kernel} 2means test \
       --hss_rel_tol=${RTOL} -hss_leaf_size=128 --hss_max_rank=2000 \
-      --hss_d0=1024 --hss_dd=512 --hss_log_ranks --hss_verbose
+      --hss_dd=${DD} --hss_verbose
+
+RTOL=1e-2
+DD=128
+time ${EXEC} ${DATA} ${dim} ${hval} ${lval} ${kernel} 2means test \
+      --hss_rel_tol=${RTOL} -hss_leaf_size=128 --hss_max_rank=2000 \
+      --hss_dd=${DD} --hss_verbose
+
+RTOL=1e-3
+DD=512
+time ${EXEC} ${DATA} ${dim} ${hval} ${lval} ${kernel} 2means test \
+      --hss_rel_tol=${RTOL} -hss_leaf_size=128 --hss_max_rank=2000 \
+      --hss_dd=${DD} --hss_verbose
