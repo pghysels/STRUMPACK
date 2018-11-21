5c1cc584c684218a29db3b298dc85702617cdcd5
Pieter Ghysels <pghysels@lbl.gov>
2018-11-20 16:55:25 -0800

    Remove some test code.

--- src/sparse/FrontalMatrixHODLR.hpp
+++ src/sparse/FrontalMatrixHODLR.hpp
@@ -399,26 +399,9 @@ namespace strumpack {
     CSRMatrix<scalar_t,integer_t> A11, A12, A21;
     extract_sparse(A, A11, A12, A21);
 
-    //// ------ temporary test code ------------------------------
-    // DenseM_t dF11(dsep, dsep); dF11.zero();
-    // DenseM_t dF12(dsep, dupd); dF12.zero();
-    // DenseM_t dF21(dupd, dsep); dF21.zero();
-    // DenseM_t dF22(dupd, dupd); dF22.zero();
-    // A.extract_front
-    //   (dF11, dF12, dF21, this->sep_begin_, this->sep_end_,
-    //    this->upd_, task_depth);
-    // if (lchild_)
-    //   lchild_->extend_add_to_dense
-    //     (dF11, dF12, dF21, dF22, this, task_depth);
-    // if (rchild_)
-    //   rchild_->extend_add_to_dense
-    //     (dF11, dF12, dF21, dF22, this, task_depth);
-    //// ---------------------------------------------------------
-
     auto sample_F11 = [&](char op, const DenseM_t& R, DenseM_t& S) {
       TIMER_TIME(TaskType::RANDOM_SAMPLING, 0, t_sampling);
       auto tr = c2T(op);
-      //gemm(tr, Trans::N, scalar_t(1.), dF11, R, scalar_t(0.), S, task_depth);
       A11.spmv(tr, R, S);
       if (lchild_) lchild_->sample_CB_to_F11(tr, R, S, this, task_depth);
       if (rchild_) rchild_->sample_CB_to_F11(tr, R, S, this, task_depth);
@@ -428,7 +411,6 @@ namespace strumpack {
       (char op, scalar_t a, const DenseM_t& R, scalar_t b, DenseM_t& S) {
       TIMER_TIME(TaskType::RANDOM_SAMPLING, 0, t_sampling);
       auto tr = c2T(op);
-      //gemm(tr, Trans::N, a, dF12, R, b, S, task_depth);
       DenseM_t lR(R), lS(S);
       lR.scale(a);
       A12.spmv(tr, lR, lS);
@@ -441,7 +423,6 @@ namespace strumpack {
       (char op, scalar_t a, const DenseM_t& R, scalar_t b, DenseM_t& S) {
       TIMER_TIME(TaskType::RANDOM_SAMPLING, 0, t_sampling);
       auto tr = c2T(op);
-      //gemm(tr, Trans::N, a, dF21, R, b, S, task_depth);
       DenseM_t lR(R), lS(S);
       lR.scale(a);
       A21.spmv(tr, lR, lS);
@@ -461,26 +442,10 @@ namespace strumpack {
     F11_.factor();
     TIMER_STOP(t_fact);
 
-    //// ------ temporary test code -----------------------------
-    // auto piv = dF11.LU(task_depth);
     if (dupd) {
-      // dF12.laswp(piv, true);
-      // trsm(Side::L, UpLo::L, Trans::N, Diag::U,
-      //      scalar_t(1.), dF11, dF12, task_depth);
-      // trsm(Side::R, UpLo::U, Trans::N, Diag::N,
-      //      scalar_t(1.), dF11, dF21, task_depth);
-
-      // BETA=0 means dF22 has only the Schur update
-      // BETA=1 means dF22 has the full Schur complement
-      // gemm(Trans::N, Trans::N, scalar_t(-1.), dF21, dF12,
-      //      scalar_t(0.), dF22, task_depth);
-      //// ---------------------------------------------------------
-
       auto sample_CB = [&](char op, const DenseM_t& R, DenseM_t& S) {
         TIMER_TIME(TaskType::RANDOM_SAMPLING, 0, t_sampling);
         auto tr = c2T(op);
-        // gemm(tr, Trans::N, scalar_t(1.), dF22, R, scalar_t(0.), S, task_depth);
-        // S.zero();
         DenseM_t F12R(dsep, R.cols()), invF11F12R(dsep, R.cols());
         if (tr == Trans::N) {
           F12_.mult(tr, scalar_t(1.), R, scalar_t(0.), F12R);
