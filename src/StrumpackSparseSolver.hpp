6d3eda62622cb37aa6af4d3e9d208136fcd1d975
Michael Neuder <michael.neuder@gmail.com>
2020-11-12 10:05:16 -0500

    Revert

--- src/StrumpackSparseSolver.hpp
+++ src/StrumpackSparseSolver.hpp
@@ -204,7 +204,7 @@ namespace strumpack {
      */
     void update_matrix_values(const CSRMatrix<scalar_t,integer_t>& A);
 
-  protected:
+  private:
     void setup_tree() override;
     void setup_reordering() override;
     int compute_reordering(const int* p, int base,
