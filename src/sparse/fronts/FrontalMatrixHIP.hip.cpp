3fa86e372e58a3b999bf7ff985f501f6f688e1ea
Pieter Ghysels <pghysels@lbl.gov>
2021-12-17 02:33:17 -0500

    Update assemble and extend add HIP kernels, based on NERSC hackathon work for NVIDIA

--- src/sparse/fronts/FrontalMatrixHIP.hip.cpp
+++ src/sparse/fronts/FrontalMatrixHIP.hip.cpp
@@ -136,45 +136,31 @@ namespace strumpack {
 
 
     /**
-     * Put elements of the sparse matrix in the F11 part of the front.
-     * The sparse elements are taken from F.e11, which is a list of
-     * triplets {r,c,v}. The front is assumed to be initialized to
-     * zero.
+     * Put elements of the sparse matrix in the F11, F12 and F21 parts
+     * of the front.  The sparse elements are taken from F.e11, F.e12,
+     * F.e21, which are lists of triplets {r,c,v}. The front is
+     * assumed to be initialized to zero.
      *
-     * Use this with a 1-dimensional grid, where the number of grid
-     * blocks in the x direction is the number of fronts, with f0
-     * being the first front pointed to by dat. The threadblock should
-     * also be 1d.
      */
-    template<typename T> __global__ void
-    assemble_11_kernel(unsigned int f0, AssembleData<T>* dat) {
-      int idx = hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x;
-      auto& F = dat[hipBlockIdx_y + f0];
-      if (idx >= F.n11) return;
-      auto& t = F.e11[idx];
-      F.F11[t.r + t.c*F.d1] = t.v;
-    }
-    /**
-     * Put elements of the sparse matrix in the F12 anf F21 parts of
-     * the front. These two are combined because F.n12 and F.n21 are
-     * (probably always?) equal, and to save on overhead of launching
-     * kernels/blocks.
-     *
-     * Use this with a 1-dimensional grid, where the number of grid
-     * blocks in the x direction is the number of fronts, with f0
-     * being the first front pointed to by dat. The threadblock should
-     * also be 1d.
-     */
-    template<typename T> __global__ void
-    assemble_12_21_kernel(unsigned int f0, AssembleData<T>* dat) {
-      int idx = hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x;
-      auto& F = dat[hipBlockIdx_y + f0];
-      if (idx < F.n12) {
-        auto& t = F.e12[idx];
+    template<typename T, int unroll> __global__ void
+    assemble_kernel(unsigned int f0, unsigned int nf, AssembleData<T>* dat) {
+      int idx = (hipBlockIdx_x * hipBlockDim_x) * unroll + hipThreadIdx_x,
+        op = (hipBlockIdx_y + f0) * hipBlockDim_y + hipThreadIdx_y;
+      if (op >= nf) return;
+      auto& F = dat[op];
+      for (int i=0, j=idx; i<unroll; i++, j+=hipBlockDim_x) {
+        if (j >= F.n11) break;
+        auto& t = F.e11[j];
+        F.F11[t.r + t.c*F.d1] = t.v;
+      }
+      for (int i=0, j=idx; i<unroll; i++, j+=hipBlockDim_x) {
+        if (j >= F.n12) break;
+        auto& t = F.e12[j];
         F.F12[t.r + t.c*F.d1] = t.v;
       }
-      if (idx < F.n21) {
-        auto& t = F.e21[idx];
+      for (int i=0, j=idx; i<unroll; i++, j+=hipBlockDim_x) {
+        if (j >= F.n21) break;
+        auto& t = F.e21[j];
         F.F21[t.r + t.c*F.d2] = t.v;
       }
     }
@@ -184,98 +170,101 @@ namespace strumpack {
      * Single extend-add operation from one contribution block into
      * the parent front. d1 is the size of F11, d2 is the size of F22.
      */
-    template<typename T> __device__ void
-    ea_kernel(int x, int y, int d1, int d2, int dCB,
-              T* F11, T* F12, T* F21, T* F22,
-              T* CB, std::size_t* I) {
-      if (x >= dCB || y >= dCB) return;
-      auto Ix = I[x], Iy = I[y];
-      if (Ix < d1) {
-        if (Iy < d1) F11[Iy+Ix*d1] += CB[y+x*dCB];
-        else F21[Iy-d1+Ix*d2] += CB[y+x*dCB];
+    template<typename T, unsigned int unroll>
+    __global__ void extend_add_kernel
+    (unsigned int by0, unsigned int nf, AssembleData<T>* dat, bool left) {
+      int y = hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x,
+        x0 = (hipBlockIdx_y + by0) * unroll,
+        z = hipBlockIdx_z * hipBlockDim_z + hipThreadIdx_z;
+      if (z >= nf) return;
+      auto& f = dat[z];
+      auto CB = left ? f.CB1 : f.CB2;
+      if (!CB) return;
+      auto dCB = left ? f.dCB1 : f.dCB2;
+      if (y >= dCB) return;
+      auto I = left ? f.I1 : f.I2;
+      auto Iy = I[y];
+      CB += y + x0*dCB;
+      int d1 = f.d1, d2 = f.d2;
+      int ld;
+      T* F[2];
+      if (Iy < d1) {
+        ld = d1;
+        F[0] = f.F11+Iy;
+        F[1] = f.F12+Iy-d1*d1;
       } else {
-        if (Iy < d1) F12[Iy+(Ix-d1)*d1] += CB[y+x*dCB];
-        else F22[Iy-d1+(Ix-d1)*d2] += CB[y+x*dCB];
+        ld = d2;
+        F[0] = f.F21+Iy-d1;
+        F[1] = f.F22+Iy-d1-d1*d2;
+      }
+#pragma unroll
+      for (int i=0; i<unroll; i++) {
+        int x = x0 + i;
+        if (x >= dCB) break;
+        auto Ix = I[x];
+        F[Ix >= d1][Ix*ld] += CB[i*dCB];
       }
     }
-
-    template<typename T> __global__ void
-    extend_add_kernel_left(unsigned int by0, AssembleData<T>* dat) {
-      int x = hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x,
-        y = (hipBlockIdx_y + by0) * hipBlockDim_y + hipThreadIdx_y;
-      auto& F = dat[hipBlockIdx_z];
-      if (F.CB1)
-        ea_kernel(x, y, F.d1, F.d2, F.dCB1,
-                  F.F11, F.F12, F.F21, F.F22, F.CB1, F.I1);
-    }
-    template<typename T> __global__ void
-    extend_add_kernel_right(unsigned int by0, AssembleData<T>* dat) {
-      int x = hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x,
-        y = (hipBlockIdx_y + by0) * hipBlockDim_y + hipThreadIdx_y;
-      auto& F = dat[hipBlockIdx_z];
-      if (F.CB2)
-        ea_kernel(x, y, F.d1, F.d2, F.dCB2,
-                  F.F11, F.F12, F.F21, F.F22, F.CB2, F.I2);
-    }
-
 
     template<typename T> void
     assemble(unsigned int nf, AssembleData<T>* dat,
              AssembleData<T>* ddat) {
       { // front assembly from sparse matrix
-        unsigned int nt1 = 128, nt2 = 32, nb1 = 0, nb2 = 0;
-        for (int f=0; f<nf; f++) {
-          unsigned int b = dat[f].n11 / nt1 + (dat[f].n11 % nt1 != 0);
-          if (b > nb1) nb1 = b;
-          b = dat[f].n12 / nt2 + (dat[f].n12 % nt2 != 0);
-          if (b > nb2) nb2 = b;
-          b = dat[f].n21 / nt2 + (dat[f].n21 % nt2 != 0);
-          if (b > nb2) nb2 = b;
-        }
-        for (unsigned int f=0; f<nf; f+=MAX_BLOCKS_Y) {
-          dim3 grid(nb1, std::min(nf-f, MAX_BLOCKS_Y));
-          hipLaunchKernelGGL(HIP_KERNEL_NAME(assemble_11_kernel),
-                             grid, dim3(nt1), 0, 0, f, ddat);
-        }
-        if (nb2)
-          for (unsigned int f=0; f<nf; f+=MAX_BLOCKS_Y) {
-            dim3 grid(nb2, std::min(nf-f, MAX_BLOCKS_Y));
-            hipLaunchKernelGGL(HIP_KERNEL_NAME(assemble_12_21_kernel),
-                               grid, dim3(nt2), 0, 0, f, ddat);
+        std::size_t nnz = 0;
+        for (unsigned int f=0; f<nf; f++)
+          nnz = std::max
+            (nnz, std::max(dat[f].n11, std::max(dat[f].n12, dat[f].n21)));
+        if (nnz) {
+          unsigned int nt = 512, ops = 1;
+          const int unroll = 8;
+          while (nt*unroll > nnz && nt > 8) {
+            nt /= 2;
+            ops *= 2;
           }
+          ops = std::min(ops, nf);
+          unsigned int nb = (nnz + nt*unroll - 1) / (nt*unroll),
+            nbf = (nf + ops - 1) / ops;
+          dim3 block(nt, ops);
+          for (unsigned int f=0; f<nbf; f+=MAX_BLOCKS_Y) {
+            dim3 grid(nb, std::min(nbf-f, MAX_BLOCKS_Y));
+            hipLaunchKernelGGL(HIP_KERNEL_NAME(assemble_kernel<T,unroll>),
+                               grid, block, 0, 0, f, nf, ddat+f*ops);
+          }
+        }
       }
-      hipDeviceSynchronize();
       { // extend-add
-        unsigned int nt = 16, nb = 0;
-        for (int f=0; f<nf; f++) {
-          int b = dat[f].dCB1 / nt + (dat[f].dCB1 % nt != 0);
-          if (b > nb) nb = b;
-          b = dat[f].dCB2 / nt + (dat[f].dCB2 % nt != 0);
-          if (b > nb) nb = b;
-        }
-        dim3 block(nt, nt);
-        using T_ = typename cuda_type<T>::value_type;
-        auto dat_ = reinterpret_cast<AssembleData<T_>*>(ddat);
-        for (unsigned int b1=0; b1<nb; b1+=MAX_BLOCKS_Y) {
-          int nb1 = std::min(nb-b1, MAX_BLOCKS_Y);
-          for (unsigned int f=0; f<nf; f+=MAX_BLOCKS_Z) {
-            dim3 grid(nb, nb1, std::min(nf-f, MAX_BLOCKS_Z));
-            hipLaunchKernelGGL(HIP_KERNEL_NAME(extend_add_kernel_left),
-                               grid, block, 0, 0, b1, dat_+f);
+        int du = 0;
+        for (unsigned int f=0; f<nf; f++)
+          du = std::max(du, std::max(dat[f].dCB1, dat[f].dCB2));
+        if (du) {
+          const unsigned int unroll = 16;
+          unsigned int nt = 512, ops = 1;
+          while (nt > du) {
+            nt /= 2;
+            ops *= 2;
           }
-        }
-        hipDeviceSynchronize();
-        for (unsigned int b1=0; b1<nb; b1+=MAX_BLOCKS_Y) {
-          int nb1 = std::min(nb-b1, MAX_BLOCKS_Y);
-          for (unsigned int f=0; f<nf; f+=MAX_BLOCKS_Z) {
-            dim3 grid(nb, nb1, std::min(nf-f, MAX_BLOCKS_Z));
-            hipLaunchKernelGGL(HIP_KERNEL_NAME(extend_add_kernel_right),
-                               grid, block, 0, 0, b1, dat_+f);
+          ops = std::min(ops, nf);
+          unsigned int nbx = (du + nt - 1) / nt,
+            nby = (du + unroll - 1) / unroll,
+            nbf = (nf + ops - 1) / ops;
+          dim3 block(nt, 1, ops);
+          using T_ = typename cuda_type<T>::value_type;
+          auto dat_ = reinterpret_cast<AssembleData<T_>*>(ddat);
+          for (unsigned int y=0; y<nby; y+=MAX_BLOCKS_Y) {
+            unsigned int ny = std::min(nby-y, MAX_BLOCKS_Y);
+            for (unsigned int f=0; f<nbf; f+=MAX_BLOCKS_Z) {
+              dim3 grid(nbx, ny, std::min(nbf-f, MAX_BLOCKS_Z));
+              hipLaunchKernelGGL
+                (HIP_KERNEL_NAME(extend_add_kernel<T_,unroll>),
+                 grid, block, 0, 0, y, nf, dat_+f*ops, true);
+              hipLaunchKernelGGL
+                (HIP_KERNEL_NAME(extend_add_kernel<T_,unroll>),
+                 grid, block, 0, 0, y, nf, dat_+f*ops, false);
+            }
           }
         }
       }
     }
-
 
     // /**
     //  * This only works if value >= 0.
