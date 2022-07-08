08b41c6af951256c81ba90fdc469b888eaa3ea9a
yotamyaniv <yotamya@ucla.edu>
2022-07-08 14:40:38 -0700

    added binary CRS matrix template class

--- src/HSS/HSSMatrix.Sketches.hpp
+++ src/HSS/HSSMatrix.Sketches.hpp
@@ -8,6 +8,7 @@
 #include <algorithm>
 #include <random>
 #include <chrono>
+#include <stdexcept>
 
 namespace strumpack {
   namespace HSS {
@@ -50,7 +51,8 @@ namespace strumpack {
                    col_inds.push_back(j);
                }
 
-               std::vector<scalar_t> nums = {scalar_t(1./std::sqrt(double(nnz))),
+               std::vector<scalar_t> nums = {
+                   scalar_t(1./std::sqrt(double(nnz))),
                    scalar_t(-1./std::sqrt(double(nnz)))};
 
                 for (std::size_t i=0; i<B.rows(); i++){
@@ -78,9 +80,115 @@ namespace strumpack {
      private:
           unsigned long seed_;
           std::default_random_engine e_;
-
       };
 
+
+
+      template<typename scalar_t, typename integer_t> class BinaryCRSMarix {
+      public:
+
+          BinaryCRSMarix(integer_t n_cols) {
+              n_cols_ = n_cols;
+          }
+
+          BinaryCRSMarix(std::vector<integer_t> col_ind,
+              std::vector<integer_t>row_ptr, integer_t n_cols) {
+              n_cols_ = n_cols;
+              //TODO
+              /*
+              integer_t nnz_ = row_ind.size();
+              integer_t n_cols_ = col_ptr.size() - 1;
+              std::vector<integer_t> row_ind_(row_ind);
+              std::vector<integer_t> col_ptr_(col_ptr);
+              */
+          }
+
+
+          void print() {
+              std::cout << "row ptr: ";
+              for (integer_t i = 0; i <= size(); i++)
+                  std::cout << row_ptr_[i] << " ";
+              std::cout << std::endl << "col ind: ";
+              for (integer_t i = 0; i < nnz(); i++)
+                  std::cout << col_ind_[i] << " ";
+              std::cout << std::endl;
+              std::cout << "val: " << one_ << std::endl;
+              std::cout <<"n_cols " << n_cols_ << std::endl;
+          }
+
+          void print_as_dense() {
+              for (integer_t i = 0; i < row_ptr_.size() - 1; i++) {
+                  integer_t start = row_ptr_[i] - 1, end = row_ptr_[i + 1] -1;
+
+
+                  for (integer_t j = 0; j < n_cols_; j++) {
+
+                      if (std::find(col_ind_.begin() + start, col_ind_.begin()
+                      + end, j) != col_ind_.begin() + end) {
+                          std::cout << one_ << " ";
+                      }
+                      else {
+                          std::cout << "0 ";
+                      }
+
+                  }
+                  std::cout << std::endl;
+              }
+          }
+
+          void add_row(
+              std::vector<integer_t> new_col_ind_) {
+              integer_t added_nnz_ = new_col_ind_.size();
+              //append col_inds
+              col_ind_.insert(std::end(col_ind_),
+                  std::begin(new_col_ind_), std::end(new_col_ind_));
+              //update nnz_
+              nnz_ += added_nnz_;
+              //update row_ptr
+              row_ptr_.push_back(nnz_ + 1);
+              //update n_rows
+              n_rows_ += 1;
+          }
+
+          void set_nnz_value(scalar_t one) {
+              one_ = one;
+          }
+
+          scalar_t nnz_value() {
+              return one_;
+          }
+
+          integer_t nnz() {
+              return nnz_;
+          }
+
+          integer_t size() {
+              return n_rows_;
+          }
+      private:
+          integer_t nnz_ = integer_t(0);
+          integer_t n_cols_;
+          integer_t n_rows_ = integer_t(0);
+          scalar_t one_ = scalar_t(1.);
+          std::vector<integer_t> col_ind_ = {};
+          std::vector<integer_t> row_ptr_ = { integer_t(1) };
+      };
+
+
+template<typename scalar_t,typename integer_t> class SJLT_Matrix{
+public:
+
+// Print A
+// print B
+// print sjlt
+// apply SJLT_matrix to A*SJLT
+private:
+    // 1 will be cast as scalar_t 1
+    //integer_t for size of row/column indices
+    BinaryCRSMarix< scalar_t, integer_t> A_row_wise;
+    BinaryCRSMarix< scalar_t, integer_t> B_row_wise;
+    scalar_t nnz;
+};
 
 
   }
