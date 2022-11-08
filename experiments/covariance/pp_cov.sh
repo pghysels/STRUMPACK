5a0cee0d043b27dd955e1863881e3cf5ef4d3860
Pieter Ghysels <pghysels@lbl.gov>
2022-11-07 20:29:43 -0800

    Update scripts for covariance experiment.

--- experiments/covariance/pp_cov.sh
+++ experiments/covariance/pp_cov.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
 
+# out=out_cov_old
+out=out_cov_in_place #_l256
 comp=stable
-tol=1e-4
-
-for nnz in 2 4 8; do
-    for k in 14 19 24 29 34; do
-        grep "Number of unknowns:" out_cov/out_dim3_k${k}*tol${tol}*${comp}_sjlt_nnz${nnz} | awk '{print $4}' >> tmp_N_nnz${nnz}
-        grep "rank(H)" out_cov/out_dim3_k${k}*tol${tol}*${comp}_sjlt_nnz${nnz} | awk '{print $4}' >> tmp_rank_nnz${nnz}
-        grep "A\*S time\|AT\*S time" out_cov/out_dim3_k${k}*tol${tol}*${comp}_sjlt_nnz${nnz} | awk '{sum+=$5} END {print sum}' >> tmp_sample0_time_nnz${nnz}
-        grep "A\*S appended cols time\|AT\*S appended cols time" out_cov/out_dim3_k${k}*${comp}_sjlt_nnz${nnz} | awk '{sum+=$7} END {print sum}' >> tmp_sample_append_time_nnz${nnz}
-        grep "A\*S and AT\*S gemm" out_cov/out_dim3_k${k}*tol${tol}*${comp}_sjlt_nnz${nnz} | awk '{sum+=$6} END {print sum}' >> tmp_sample_gauss_time_nnz${nnz}
-        grep "relative error" out_cov/out_dim3_k${k}*tol${tol}*${comp}_sjlt_nnz${nnz} | awk '{print $7}' >> tmp_rel_err_nnz${nnz}
+for tol in 1e-2 1e-4 1e-6; do
+    rm tmp*
+    echo "${tol}"
+    for nnz in 1 2 4 8; do
+        for k in 9 19 29; do
+            grep "Number of unknowns:" ${out}/out_dim3_k${k}*tol${tol}*${comp}_sjlt_nnz${nnz} | awk '{print $4}' >> tmp_N_nnz${nnz}
+            grep "rank(H)" ${out}/out_dim3_k${k}*tol${tol}*${comp}_sjlt_nnz${nnz} | awk '{print $4}' >> tmp_rank_nnz${nnz}
+            grep "A\*S time\|AT\*S time" ${out}/out_dim3_k${k}*tol${tol}*${comp}_sjlt_nnz${nnz} | awk '{sum+=$5} END {print sum}' >> tmp_sample0_time_nnz${nnz}
+            grep "A\*S appended cols time\|AT\*S appended cols time" ${out}/out_dim3_k${k}*tol${tol}*${comp}_sjlt_nnz${nnz} | awk '{sum+=$7} END {print sum}' >> tmp_sample_append_time_nnz${nnz}
+            grep "A\*S and AT\*S gemm" ${out}/out_dim3_k${k}*tol${tol}*${comp}_sjlt_nnz${nnz} | awk '{sum+=$6} END {print sum}' >> tmp_sample_gauss_time_nnz${nnz}
+            grep "relative error" ${out}/out_dim3_k${k}*tol${tol}*${comp}_sjlt_nnz${nnz} | awk '{print $7}' >> tmp_rel_err_nnz${nnz}
+        done
+        paste tmp_N_nnz${nnz} tmp_rank_nnz${nnz} > tmp_N_rank_nnz${nnz}
+        paste tmp_N_nnz${nnz} tmp_sample0_time_nnz${nnz} tmp_sample_append_time_nnz${nnz} tmp_sample_gauss_time_nnz${nnz} > tmp_N_sample_append_nnz${nnz}
+        cat tmp_N_sample_append_nnz${nnz} | awk '{print $2+$3}' > tmp_constr_nnz${nnz}
+        paste tmp_N_nnz${nnz} tmp_rel_err_nnz${nnz} > tmp_N_rel_err_nnz${nnz}
     done
-    paste tmp_N_nnz${nnz} tmp_rank_nnz${nnz} > tmp_N_rank_nnz${nnz}
-    paste tmp_N_nnz${nnz} tmp_sample0_time_nnz${nnz} tmp_sample_append_time_nnz${nnz} tmp_sample_gauss_time_nnz${nnz} > tmp_N_sample_append_nnz${nnz}
-    paste tmp_N_nnz${nnz} tmp_rel_err_nnz${nnz} > tmp_N_rel_err_nnz${nnz}
+    for k in 9 19 29; do
+        grep "Number of unknowns:" ${out}/out_dim3_k${k}*tol${tol}*${comp}_gaussian | awk '{print $4}' >> tmp_N_gaussian
+        grep "rank(H)" ${out}/out_dim3_k${k}*tol${tol}*${comp}_gaussian | awk '{print $4}' >> tmp_rank_gaussian
+        grep "relative error" ${out}/out_dim3_k${k}*tol${tol}*${comp}_gaussian | awk '{print $7}' >> tmp_rel_err_gaussian
+    done
+    paste tmp_N_gaussian tmp_rank_gaussian > tmp_N_rank_gaussian
+    paste tmp_N_gaussian tmp_rel_err_gaussian > tmp_N_rel_err_gaussian
+
+    paste tmp_N_gaussian tmp_sample_gauss_time_nnz2 tmp_constr_nnz1 tmp_constr_nnz2 tmp_constr_nnz4 tmp_constr_nnz8 tmp_rank_gaussian tmp_rank_nnz1 tmp_rank_nnz2 tmp_rank_nnz4 tmp_rank_nnz8 tmp_rel_err_gaussian tmp_rel_err_nnz1 tmp_rel_err_nnz2 tmp_rel_err_nnz4 tmp_rel_err_nnz8 > latex_table
+    cat latex_table
 done
-for k in 14 19 24 29 34; do
-    grep "Number of unknowns:" out_cov/out_dim3_k${k}*tol${tol}*${comp}_gaussian | awk '{print $4}' >> tmp_N_gaussian
-    grep "rank(H)" out_cov/out_dim3_k${k}*tol${tol}*${comp}_gaussian | awk '{print $4}' >> tmp_rank_gaussian
-    grep "relative error" out_cov/out_dim3_k${k}*tol${tol}*${comp}_gaussian | awk '{print $7}' >> tmp_rel_err_gaussian
-done
-paste tmp_N_gaussian tmp_rank_gaussian > tmp_N_rank_gaussian
-paste tmp_N_gaussian tmp_rel_err_gaussian > tmp_N_rel_err_gaussian
-cat tmp_N_rel_err_gaussian
 
 gnuplot plot_cov.gpl
 
--- experiments/covariance/run_cov.sh
+++ experiments/covariance/run_cov.sh
@@ -4,33 +4,38 @@ T=8
 
 dim=3
 cor=.2
-tol=1e-4
+tol=1e-2
 
-mkdir out_cov
+out=out_cov_in_place_l256
+mkdir $out
 
-leaf=64
+leaf=256
 
 export OMP_NUM_THREADS=$T
-for comp in stable original; do
-    for k in 14 19 24 29; do
-        echo $k
-        sampling=sjlt
-        for nnz in 2 4 8; do
-            echo $nnz
-            ../build/examples/dense/testCovariance \
+# for comp in stable original; do
+for tol in 1e-2 1e-4 1e-6; do
+    for comp in stable; do
+        # for k in 14 19 24 29; do
+        for k in 9 19 29; do
+            echo $k
+            sampling=sjlt
+            for nnz in 1 2 4 8; do
+                echo $nnz
+                ../../build/examples/dense/testCovariance \
+                    $dim $k $cor --hss_rel_tol $tol \
+                    --hss_compression_algorithm $comp \
+                    --hss_nnz0 $nnz --hss_nnz $nnz \
+                    --hss_compression_sketch $sampling \
+                    --hss_leaf_size $leaf --help \
+                    > ${out}/out_dim${dim}_k${k}_cor${cor}_T${T}_tol${tol}_leaf${leaf}_${comp}_${sampling}_nnz${nnz}
+            done
+            sampling=gaussian
+            ../../build/examples/dense/testCovariance \
                 $dim $k $cor --hss_rel_tol $tol \
                 --hss_compression_algorithm $comp \
-                --hss_nnz0 $nnz --hss_nnz $nnz \
                 --hss_compression_sketch $sampling \
                 --hss_leaf_size $leaf --help \
-                > out_cov/out_dim${dim}_k${k}_cor${cor}_T${T}_tol${tol}_leaf${leaf}_${comp}_${sampling}_nnz${nnz}
+                > ${out}/out_dim${dim}_k${k}_cor${cor}_T${T}_tol${tol}_leaf${leaf}_${comp}_${sampling}
         done
-        sampling=gaussian
-        ../build/examples/dense/testCovariance \
-            $dim $k $cor --hss_rel_tol $tol \
-            --hss_compression_algorithm $comp \
-            --hss_compression_sketch $sampling \
-            --hss_leaf_size $leaf --help \
-            > out_cov/out_dim${dim}_k${k}_cor${cor}_T${T}_tol${tol}_leaf${leaf}_${comp}_${sampling}
     done
 done
