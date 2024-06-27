b19b6aae6b2360d9e1def4041d7e34bdce7e6e62
Pieter Ghysels <pghysels@lbl.gov>
2024-06-27 12:14:40 -0700

    Add search path for ScaLAPACK

--- cmake/Modules/FindScaLAPACK.cmake
+++ cmake/Modules/FindScaLAPACK.cmake
@@ -1,11 +1,11 @@
 if (NOT (TPL_SCALAPACK_LIBRARIES))
   find_library(SCALAPACK_LIBRARY
     NAMES scalapack scalapack-pvm scalapack-mpi scalapack-mpich scalapack-mpich2 scalapack-openmpi scalapack-lam
-    PATHS /usr/lib64 /usr/lib /usr/local/lib64 /usr/local/lib /sw/lib /usr/lib64/openmpi/lib /usr/lib64/mpich/lib
+    PATHS /usr/lib64 /usr/lib /usr/local/lib64 /usr/local/lib /sw/lib /usr/lib64/openmpi/lib /usr/lib64/mpich/lib /usr/lib/x86_64-linux-gnu/
     ENV LD_LIBRARY_PATH ENV SCALAPACKDIR ENV BLACSDIR)
   find_library(BLACS_LIBRARY
     NAMES blacs blacs-pvm blacs-mpi blacs-mpich blacs-mpich2 blacs-openmpi blacs-lam mpiblacs scalapack ${SCALAPACK_LIBRARY}
-    PATHS /usr/lib64 /usr/lib /usr/local/lib64 /usr/local/lib /sw/lib /usr/lib64/openmpi/lib /usr/lib64/mpich/lib
+    PATHS /usr/lib64 /usr/lib /usr/local/lib64 /usr/local/lib /sw/lib /usr/lib64/openmpi/lib /usr/lib64/mpich/lib /usr/lib/x86_64-linux-gnu/
     ENV LD_LIBRARY_PATH ENV SCALAPACKDIR ENV BLACSDIR)
   set(SCALAPACK_LIBRARIES ${SCALAPACK_LIBRARY} ${BLACS_LIBRARY})
 else()
